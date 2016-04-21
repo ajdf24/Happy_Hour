9b603a5907e683cf16cc2c3263c25dd99f1c66d0
sebastian <sebastian2406@t-online.de>
2016-04-21 18:20:48 +0200

    Initale Grundactivitys hinzugefügt

---
+++ app/src/main/java/it/rieger/happyhour/LocationList.java
@@ -0,0 +1,13 @@
+package it.rieger.happyhour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_list);
+    }
+}
