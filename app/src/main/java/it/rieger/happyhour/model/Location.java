f6076d27a9e9db960175f957aab09d8da4dc6532
sebastian <sebastian2406@t-online.de>
2016-04-25 15:27:33 +0200

    Bilder Cache in Location klasse eingearbeitet.

--- app/src/main/java/it/rieger/happyhour/model/Location.java
+++ app/src/main/java/it/rieger/happyhour/model/Location.java
@@ -1,8 +1,10 @@
 package it.rieger.happyhour.model;
 
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import it.rieger.happyhour.controller.BitmapLRUCache;
@@ -10,7 +12,6 @@ import it.rieger.happyhour.controller.BitmapLRUCache;
 /**
  * Model class, which represents a Location, which all the data which are came from the the database.
  *
- * TODO: Wir müssen uns noch gedanken machen, wie wir die Bilder speichern. Ich würde einen cache vorschlagen, so das Bilder nicht immer neu geladen werden müssen.
  * Created by sebastian on 25.04.16.
  */
 public class Location implements Serializable{
@@ -51,6 +52,38 @@ public class Location implements Serializable{
     public List<HappyHour> happyHours;
 
     /**
+     * list of image keys for the location
+     */
+    public List<String> imageKeyList = new ArrayList<>();
+
+    /**
+     * list of image keys which are not in the cache
+     */
+    public List<String> notCachedImages = new ArrayList<>();
+
+    /**
+     * constructor
+     * @param name name of the location
+     * @param rating rating of the location
+     * @param addressName address name of the location
+     * @param addressLongitude longitude of the address
+     * @param addressLatitude latitude of the address
+     * @param openingTimes opening times of the location
+     * @param happyHours happy hours
+     * @param imageKeyList image keys of this location
+     */
+    public Location(@NonNull String name, @NonNull float rating, @NonNull String addressName, @NonNull float addressLongitude, @NonNull float addressLatitude, OpeningTimes openingTimes, List<HappyHour> happyHours, List<String> imageKeyList) {
+        this.name = name;
+        this.rating = rating;
+        this.addressName = addressName;
+        this.addressLongitude = addressLongitude;
+        this.addressLatitude = addressLatitude;
+        this.openingTimes = openingTimes;
+        this.happyHours = happyHours;
+        this.imageKeyList = imageKeyList;
+    }
+
+    /**
      * constructor
      * @param name name of the location
      * @param rating rating of the location
@@ -61,13 +94,7 @@ public class Location implements Serializable{
      * @param happyHours happy hours
      */
     public Location(@NonNull String name, @NonNull float rating, @NonNull String addressName, @NonNull float addressLongitude, @NonNull float addressLatitude, OpeningTimes openingTimes, List<HappyHour> happyHours) {
-        this.name = name;
-        this.rating = rating;
-        this.addressName = addressName;
-        this.addressLongitude = addressLongitude;
-        this.addressLatitude = addressLatitude;
-        this.openingTimes = openingTimes;
-        this.happyHours = happyHours;
+        this(name, rating, addressName, addressLongitude, addressLatitude, openingTimes, null, null);
     }
 
     /**
@@ -80,7 +107,7 @@ public class Location implements Serializable{
      * @param openingTimes opening times of the location
      */
     public Location(@NonNull String name, @NonNull float rating, @NonNull String addressName, @NonNull float addressLongitude, @NonNull float addressLatitude, OpeningTimes openingTimes) {
-        this(name, rating, addressName, addressLongitude, addressLatitude, openingTimes, null);
+        this(name, rating, addressName, addressLongitude, addressLatitude, openingTimes, null, null);
     }
 
     /**
@@ -92,7 +119,7 @@ public class Location implements Serializable{
      * @param addressLatitude latitude of the address
      */
     public Location(@NonNull String name, @NonNull float rating, @NonNull String addressName, @NonNull float addressLongitude, @NonNull float addressLatitude) {
-        this(name, rating, addressName, addressLongitude, addressLatitude, null, null);
+        this(name, rating, addressName, addressLongitude, addressLatitude, null, null, null);
     }
 
     public String getName() {
@@ -149,6 +176,56 @@ public class Location implements Serializable{
 
     public void setHappyHours(List<HappyHour> happyHours) {
         this.happyHours = happyHours;
+    }
+
+    public List<String> getImageKeyList() {
+        return imageKeyList;
+    }
+
+    public void setImageKeyList(List<String> imageKeyList) {
+        this.imageKeyList = imageKeyList;
+    }
+
+    public void setImageKey(String key){
+        imageKeyList.add(key);
+    }
+
+    /**
+     * Return all cached images for a specific location.
+     * @return a list of {@link Bitmap} with the images which are in the cache
+     */
+    public List<Bitmap> getCachedImages(){
+        List<Bitmap> imageList = new ArrayList<>();
+
+        for(String key : imageKeyList){
+            Bitmap image = BitmapLRUCache.getInstance().getBitmapFromMemCache(key);
+            if(image != null) {
+                imageList.add(image);
+            }else {
+                notCachedImages.add(key);
+            }
+        }
+
+        return imageList;
+    }
+
+    /**
+     * Get a list of all keys of images which are not in the cache
+     * @return
+     */
+    public List<String> getNotCachedImages(){
+        List<Bitmap> imageList = new ArrayList<>();
+
+        for(String key : imageKeyList){
+            Bitmap image = BitmapLRUCache.getInstance().getBitmapFromMemCache(key);
+            if(image != null) {
+                imageList.add(image);
+            }else {
+                notCachedImages.add(key);
+            }
+        }
+
+        return notCachedImages;
     }
 
     @Override
