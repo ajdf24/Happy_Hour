b43ab8dde623fc2a916692e8a4f353fc7cdcd621
XKain89X <renekatzung@arcor.de>
2016-04-29 13:02:10 +0200

    Facebook login begonnen

--- app/src/main/java/it/rieger/happyhour/view/MainActivity.java
+++ app/src/main/java/it/rieger/happyhour/view/MainActivity.java
@@ -1,19 +1,14 @@
 package it.rieger.happyhour.view;
 
-import android.support.v4.app.FragmentActivity;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
+
+
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
+
 
 import it.rieger.happyhour.R;
 import it.rieger.happyhour.controller.cache.BitmapLRUCache;
--- app/src/main/java/it/rieger/happyhour/view/MainActivityFragment.java
+++ app/src/main/java/it/rieger/happyhour/view/MainActivityFragment.java
@@ -3,6 +3,9 @@ package it.rieger.happyhour.view;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.util.Log;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -10,6 +13,7 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
 
 /**
  * Created by admin on 29.04.16.
@@ -25,8 +29,7 @@ public class MainActivityFragment extends FragmentActivity {
         LoginButton loginButton = (LoginButton) findViewById(R.id.usersettings_fragment_login_Button);
 
         @Override
-        public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle
-        saveInstanceState){
+        public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
             View view = inflater.inflate(R.layout.splash, container, false);
 
             loginButton = (LoginButton) view.findViewById(R.id.login_button);
