9db0ef5ffe7b9bad64631bae5b5f3e7f9ad6f123
sebastian <sebastian2406@t-online.de>
2016-05-01 17:40:53 +0200

    Fehler im Facebook login korrigiert

--- app/src/main/java/it/rieger/happyhour/view/MainActivity.java
+++ app/src/main/java/it/rieger/happyhour/view/MainActivity.java
@@ -1,27 +1,74 @@
 package it.rieger.happyhour.view;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 
-
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import it.rieger.happyhour.R;
 import it.rieger.happyhour.controller.cache.BitmapLRUCache;
 
 public class MainActivity extends AppCompatActivity {
 
+    CallbackManager callbackManager;
+
+    @Bind(R.id.login_button)
+    LoginButton loginButton;
+
+    @Bind(R.id.info)
+    TextView info;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Log.i("Test",BitmapLRUCache.BITMAP_LRU_CACHE.name());
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.setReadPermissions("user_friends");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
 
