05d0908ac2207b4b8a2833a440513d03488df111
sebastian <sebastian2406@t-online.de>
2016-06-08 09:13:19 +0200

    Camerafragment für Rene vorbereitet

---
+++ app/src/main/java/it/rieger/happyhour/view/fragments/changelocation/CameraFragment.java
@@ -0,0 +1,122 @@
+package it.rieger.happyhour.view.fragments.changelocation;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import it.rieger.happyhour.R;
+import it.rieger.happyhour.model.Location;
+import it.rieger.happyhour.view.ChangeLocationActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CameraFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CameraFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CameraFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+
+    private Location mParam1;
+
+    private OnFragmentInteractionListener mListener;
+//    TODO:
+//    @Bindpü(R.id.irgendwas)
+//    Button irgendwas;
+
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment CameraFragment.
+     */
+    public static CameraFragment newInstance(Location param1) {
+        CameraFragment fragment = new CameraFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = (Location) getArguments().getSerializable(ARG_PARAM1);
+        }
+//        TODO:
+        //irgendwas.setOnClickListener(new View.OnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            Intent für Bilder
+//                bla
+//        }
+//    });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
